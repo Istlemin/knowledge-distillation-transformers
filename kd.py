93e6ddddd318c097881bc3130a8d5f47c58608d0
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2023-03-19 12:28:14 -0500

    .

--- kd.py
+++ kd.py
@@ -40,80 +40,100 @@ class KDPred(KDLoss):
         return soft_cross_entropy(student_output.logits, teacher_output.logits)
 
 
-class KDAttention(KDLoss):
-    def __init__(
-        self,
-        teacher_cfg: BertConfig,
-        student_cfg: BertConfig,
-        layer_map="uniform",
-    ):
-        super().__init__()
-        self.layer_map = torch.zeros((student_cfg.num_hidden_layers,teacher_cfg.num_hidden_layers))
-        if layer_map == "uniform":
-            # Evenly spread out layer map,
-            for i in range(student_cfg.num_hidden_layers):
-                self.layer_map[i,
-                    (i + 1)
-                    * (teacher_cfg.num_hidden_layers)
-                    // (student_cfg.num_hidden_layers)
-                    - 1
-                ] = 10000000
-            self.layer_map = nn.Parameter(self.layer_map)
-            self.layer_map.requires_grad_(False)
-        elif layer_map == "beginning":
-            self.layer_map = list(range(student_cfg.num_hidden_layers))
-        elif layer_map == "learned":
-            for i in range(teacher_cfg.num_hidden_layers):
-                self.layer_map[i * (student_cfg.num_hidden_layers) // (teacher_cfg.num_hidden_layers),i] = 2.0
+class LayerMap(nn.Module):
+    pass
 
-            self.layer_map = nn.Parameter(self.layer_map)
+
+class ConstantLayerMap(LayerMap):
+    def __init__(self, student_layers, teacher_layers, map_type="uniform"):
+        super().__init__()
+        if map_type == "uniform_start_0":
+            # Evenly spread out layer map
+            self.layer_map = [
+                i * (teacher_layers) // (student_layers)
+                for i in range(student_layers + 1)
+            ]
+        elif map_type == "uniform":
+            # Evenly spread out layer map
+            self.layer_map = [
+                (i + 1) * (teacher_layers) // (student_layers) - 1
+                for i in range(student_layers)
+            ]
+        elif map_type == "beginning":
+            # Map student the first layers of teacher
+            self.layer_map = list(range(student_layers + 1))
         else:
             raise Exception("No such layer map implemented")
 
+    def forward(self, teacher_data, layer_index):
+        return teacher_data[self.layer_map[layer_index]]
+
+
+class LinearLayerMap(LayerMap):
+    def __init__(self, student_layers, teacher_layers, initialisation=None):
+        super().__init__()
+        self.layer_map = torch.ones((student_layers, teacher_layers))
+        if initialisation == "uniform":
+            for i in range(student_layers):
+                self.layer_map[
+                    i, (i + 1) * (teacher_layers) // (student_layers) - 1
+                ] = 10000000
+        elif initialisation == "binned":
+            for i in range(teacher_layers):
+                self.layer_map[
+                    i * (student_layers) // (teacher_layers), i
+                ] = 10000000
+        elif initialisation is not None:
+            raise Exception("No such layer map implemented")
+        self.layer_map = nn.Parameter(self.layer_map)
+
+    def forward(self, teacher_data, layer_index):
+        lmap = F.softmax(self.layer_map[layer_index],dim=0).reshape(
+            (-1,) + tuple(1 for _ in range(len(teacher_data[0].shape)))
+        )
+        return torch.sum(teacher_data * lmap.reshape((-1, 1)), dim=0)
+
+
+class KDAttention(KDLoss):
+    def __init__(self, student_cfg: BertConfig, teacher_cfg: BertConfig, layer_map):
+        super().__init__()
+        self.layer_map = layer_map
+
     def forward(self, teacher_output: ModelOutput, student_output: ModelOutput):
         teacher_atts = torch.stack(teacher_output.attentions)
-        transformed_teacher_atts = [
-            torch.sum((teacher_atts > -1e2)*teacher_atts*F.softmax(lmap.reshape((-1,1,1,1,1)),dim=0),dim=0) for lmap in self.layer_map
-        ]
+        teacher_atts = (teacher_atts > -1e2) * teacher_atts
+
         loss = 0
-        for student_att, teacher_att in zip(
-            student_output.attentions, transformed_teacher_atts
-        ):
+        for i, student_att in enumerate(student_output.attentions):
             student_att = student_att * (student_att > -1e2)
-            teacher_att = teacher_att * (teacher_att > -1e2)
+            teacher_att = self.layer_map(teacher_atts, i)
             loss += F.mse_loss(student_att, teacher_att)
         return loss
 
 
 class KDHiddenStates(KDLoss):
     def __init__(
-        self, teacher_cfg: BertConfig, student_cfg: BertConfig, layer_map="uniform"
+        self, student_cfg: BertConfig, teacher_cfg, layer_map, transform_per_layer=False
     ):
         super().__init__()
 
-        if layer_map == "uniform":
-            # Evenly spread out layer map
-            self.layer_map = [
-                i * (teacher_cfg.num_hidden_layers) // (student_cfg.num_hidden_layers)
-                for i in range(student_cfg.num_hidden_layers + 1)
-            ]
-        elif layer_map == "beginning":
-            # Evenly spread out layer map
-            self.layer_map = list(range(student_cfg.num_hidden_layers + 1))
-        else:
-            raise Exception("No such layer map implemented")
-
+        student_layers = student_cfg.num_hidden_layers + 1
         if student_cfg.hidden_size == teacher_cfg.hidden_size:
-            self.student_to_teacher = [None]*len(self.layer_map)
+            self.student_to_teacher = [None] * student_layers
         else:
-            # self.student_to_teacher = nn.Linear(student_cfg.hidden_size, teacher_cfg.hidden_size)
+            if transform_per_layer:
+                self.student_to_teacher = nn.ModuleList(
+                    [
+                        nn.Linear(student_cfg.hidden_size, teacher_cfg.hidden_size)
+                        for i in range(student_layers)
+                    ]
+                )
+            else:
+                self.student_to_teacher = nn.ModuleList(
+                    [nn.Linear(student_cfg.hidden_size, teacher_cfg.hidden_size)]
+                    * student_layers
+                )
 
-            self.student_to_teacher = nn.ModuleList(
-                [
-                    nn.Linear(student_cfg.hidden_size, teacher_cfg.hidden_size)
-                    for i in range(len(self.layer_map))
-                ]
-            )
             # set_random_seed(0)
             # self.student_to_teacher.weight = torch.nn.Parameter((torch.rand((768, 312))*2-1)*0.05)
             # self.student_to_teacher.bias = torch.nn.Parameter(torch.rand((768,))*2-1)
@@ -135,8 +155,24 @@ class KDTransformerLayers(KDLoss):
     def __init__(self, teacher_cfg: BertConfig, student_cfg: BertConfig):
         super().__init__()
 
-        self.kd_hidden_states = KDHiddenStates(teacher_cfg, student_cfg,layer_map="uniform")
-        self.kd_attention = KDAttention(teacher_cfg, student_cfg,layer_map="uniform")
+        self.kd_hidden_states = KDHiddenStates(
+            teacher_cfg,
+            student_cfg,
+            layer_map=ConstantLayerMap(
+                student_cfg.num_hidden_layers,
+                teacher_cfg.num_hidden_layers,
+                map_type="uniform_start_0",
+            ),
+        )
+        self.kd_attention = KDAttention(
+            teacher_cfg,
+            student_cfg,
+            layer_map=ConstantLayerMap(
+                student_cfg.num_hidden_layers,
+                teacher_cfg.num_hidden_layers,
+                map_type="uniform",
+            ),
+        )
 
     def forward(self, teacher_output: ModelOutput, student_output: ModelOutput):
         attention_loss = self.kd_attention(teacher_output, student_output)
@@ -256,19 +292,23 @@ def print_model(
         print(model.bert.embeddings.word_embeddings.weight.grad.view(torch.long).sum())
         print(
             model.bert.encoder.layer[0]
-            .attention.self.query.weight.grad.view(torch.long).sum()
+            .attention.self.query.weight.grad.view(torch.long)
+            .sum()
         )
         print(
             model.bert.encoder.layer[0]
-            .intermediate.dense.weight.grad.view(torch.long).sum()
+            .intermediate.dense.weight.grad.view(torch.long)
+            .sum()
         )
         print(
             model.bert.encoder.layer[-1]
-            .attention.self.query.weight.grad.view(torch.long).sum()
+            .attention.self.query.weight.grad.view(torch.long)
+            .sum()
         )
         print(
             model.bert.encoder.layer[-1]
-            .intermediate.dense.weight.grad.view(torch.long).sum()
+            .intermediate.dense.weight.grad.view(torch.long)
+            .sum()
         )
         print(model.bert.pooler.dense.weight.grad.view(torch.long).sum())
         print(model.classifier.weight.grad.view(torch.long).sum())
@@ -303,17 +343,17 @@ class KD_SequenceClassification(ModelWithLoss):
 
     def forward(self, epoch=-1, **batch):
         self.teacher.eval()
-        
-        #set_random_seed(0)
+
+        # set_random_seed(0)
         student_output = self.student(
             return_dict=True, output_hidden_states=True, output_attentions=True, **batch
         )
-        #set_random_seed(0)
+        # set_random_seed(0)
         teacher_output = self.teacher(
             return_dict=True, output_hidden_states=True, output_attentions=True, **batch
         )
 
-        #print_model(self.student,batch["input_ids"],student_output.hidden_states,student_output.attentions,student_output.logits,grad=False)
+        # print_model(self.student,batch["input_ids"],student_output.hidden_states,student_output.attentions,student_output.logits,grad=False)
 
         predictions = torch.argmax(student_output.logits, dim=1)
 
@@ -328,10 +368,47 @@ class KD_SequenceClassification(ModelWithLoss):
                 # hack to make sure all losses are part of loss computation,
                 # needed for DistributedDataParallell
                 loss += 0 * curr_loss
-            #print(curr_loss.item())
+            # print(curr_loss.item())
 
-        #print(loss.item())
+        # print(loss.item())
         return loss, predictions  # , student_output, teacher_output
 
     def save(self, path):
         torch.save(self.student, path)
+
+def tensor_equal_eps(a:torch.Tensor,b:torch.Tensor,eps:float = 1e-9):
+    return ((a-b)<eps).all
+
+def test_linear_layer_map():
+    student_layers = 3
+    teacher_layers = 6
+
+    layer_map = LinearLayerMap(3,6,initialisation=None)
+    assert tensor_equal_eps(layer_map(torch.eye(6).float(), 0), torch.tensor([1/6,1/6,1/6,1/6,1/6,1/6]).float())
+    
+    layer_map = LinearLayerMap(3,6,initialisation="uniform")
+    assert tensor_equal_eps(layer_map(torch.eye(6).float(), 0), torch.tensor([0,1,0,0,0,0]).float())
+    assert tensor_equal_eps(layer_map(torch.eye(6).float(), 2), torch.tensor([0,0,0,0,0,1]).float())
+    
+    layer_map = LinearLayerMap(3,6,initialisation="binned")
+    assert tensor_equal_eps(layer_map(torch.eye(6).float(), 0), torch.tensor([1/2,1/2,0,0,0,0]).float())
+    assert tensor_equal_eps(layer_map(torch.eye(6).float(), 2), torch.tensor([0,0,0,0,1/2,1/2]).float())
+
+def test_kd_hidden_state():
+    from model import get_bert_config
+    teacher_cfg = get_bert_config("base")
+    student_cfg = get_bert_config("TinyBERT")
+
+    KDHiddenStates(
+        teacher_cfg,
+        student_cfg,
+        layer_map=ConstantLayerMap(
+            student_cfg.num_hidden_layers,
+            teacher_cfg.num_hidden_layers,
+            map_type="uniform_start_0",
+        ),
+    )
+
+    teacher_output = 
+if __name__=="__main__":
+    test_linear_layer_map()
